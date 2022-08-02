7db68f976d7f75de9a09df99c7656daa6a6d2b24
Mithileshumate <104211685+Mithileshumate@users.noreply.github.com>
2022-08-02 13:53:16 +0530

    Add files via upload

---
+++ EC_Day-1.js
@@ -0,0 +1,43 @@
+/*
+Que-1.What is javascript?
+Ans--> Javascript is an scripting language that allows you to implement complex feature on web page .
+Javascript is an single threaded Synchronous language 
+Javascrpipt is an interpeter language that means where our code is going to run line by line.
+
+Single threaded --> one code wil be executed line by line
+synchronous-- > after excuting the first line it will be forwarded to next line
+
+compiler-- whole code will be executed at once time(C,C++,)
+interpeter -- interpeter code is going to executed line by line (Java,python,)
+javascript is an combination of both compiler and interpeter ....compiler is used to optimization in the code
+
+combination of both compiler and interpeter is known as JIT(just in time)compilation
+
+Que-2.what is javascript run time environment?
+In the run time environment  there is having browser,JS Engine, APIS,Event-loop,Callback-queue.
+inside the js engine there are 4 parts,
+i.Parser
+ii.AST
+iii.Compilation
+iv.Execution
+
+
+i.Parser-->  Parser will check the syntax in the code which is correct or not
+ii.AST--> (Abstract Syntax tree)  
+iii.Compilation--> Whole code is convert into the machine level language
+
+JAvascript engine -
+Mozilla Fire fox -- Spider monkey
+Internet Ex.  -- Chakra
+Chrome-- V8 Engine --> it having an efficiency and speed & it is very popular 
+
+
+Que-4.What is execution Context?
+Ans--> 
+In the Global execution context it will goes to into 2 parts,
+Memory creation phase & Code execution phase
+inside the Code execution phase there are 2 phase ,
+memory creation phase & code exection phase
+
+Flow of code Execution--
+*/
